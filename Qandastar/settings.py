6b467b0db3b95d6fac546c5c7d8ac40d6044b8a4
ganur2004 <124678695+ganur2004@users.noreply.github.com>
2024-04-11 09:24:32 +0500

    Update settings.py

--- Qandastar/settings.py
+++ Qandastar/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+STATIC_ROOT = [
     os.path.join(BASE_DIR, 'website/static'),
 ]
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'website/static'),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
